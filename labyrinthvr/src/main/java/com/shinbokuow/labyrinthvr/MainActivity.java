89406317ad84e237402ecfa4d4c4bcb696e74480
wyfcyx <wyfcyx@gmail.com>
2019-10-02 10:18:42 +0800

    create a minimal developing model according to sdk-hellovr project

---
+++ labyrinthvr/src/main/java/com/shinbokuow/labyrinthvr/MainActivity.java
@@ -0,0 +1,81 @@
+package com.shinbokuow.labyrinthvr;
+
+import android.os.Bundle;
+
+import com.google.vr.ndk.base.Properties;
+import com.google.vr.ndk.base.Properties.PropertyType;
+import com.google.vr.ndk.base.Value;
+import com.google.vr.sdk.audio.GvrAudioEngine;
+import com.google.vr.sdk.base.AndroidCompat;
+import com.google.vr.sdk.base.Eye;
+import com.google.vr.sdk.base.GvrActivity;
+import com.google.vr.sdk.base.GvrView;
+import com.google.vr.sdk.base.HeadTransform;
+import com.google.vr.sdk.base.Viewport;
+
+import javax.microedition.khronos.egl.EGLConfig;
+
+public class MainActivity extends GvrActivity implements GvrView.StereoRenderer {
+
+    private GvrAudioEngine gvrAudioEngine;
+
+    private Properties gvrProperties;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        initialGvrView();
+
+        gvrAudioEngine = new GvrAudioEngine(
+                this,
+                GvrAudioEngine.RenderingMode.BINAURAL_HIGH_QUALITY
+        );
+    }
+
+    public void initialGvrView() {
+        setContentView(R.layout.activity_main);
+
+        GvrView gvrView = (GvrView) findViewById(R.id.gvr_view);
+        gvrView.setEGLConfigChooser(8, 8, 8, 8, 16, 8);
+
+        gvrView.setRenderer(this);
+        gvrView.setTransitionViewEnabled(true);
+
+        gvrView.enableCardboardTriggerEmulation();
+
+        if (gvrView.setAsyncReprojectionEnabled(true)) {
+            AndroidCompat.setSustainedPerformanceMode(this, true);
+        }
+
+        setGvrView(gvrView);
+        gvrProperties = gvrView.getGvrApi().getCurrentProperties();
+    }
+    @Override
+    public void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    public void onRendererShutdown() {}
+
+    @Override
+    public void onSurfaceChanged(int width, int height) {}
+
+    @Override
+    public void onSurfaceCreated(EGLConfig config) {}
+
+    @Override
+    public void onNewFrame(HeadTransform headTransform) {}
+
+    @Override
+    public void onDrawEye(Eye eye) {}
+
+    @Override
+    public void onFinishFrame(Viewport viewport) {}
+}
